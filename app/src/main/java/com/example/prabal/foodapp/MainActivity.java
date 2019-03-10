80394b7f99ecad2e6248322073f1f8c5e0a9adcd
Prabal Srivastava <46544111+PrabalSrivastava@users.noreply.github.com>
2019-03-10 15:47:54 +0530

    First Commit

---
+++ app/src/main/java/com/example/prabal/foodapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.prabal.foodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.ic_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.refresh:
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
